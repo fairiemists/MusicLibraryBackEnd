e1ea99e7d604986b3bfb7cf070282f63b8916b5a
fairiemists <v.deweyness@gmail.com>
2022-10-23 09:32:47 -0700

    Working on Songs Model + Created Song App + Created Serializer Class + File

---
+++ songs/models.py
@@ -0,0 +1,14 @@
+from email.policy import default
+from pyexpat import model
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=255)
+
