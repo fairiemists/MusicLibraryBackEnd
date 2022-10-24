8e986cb4583f2bcf555add9c3fc5153880282083
fairiemists <v.deweyness@gmail.com>
2022-10-24 12:40:45 -0700

    Updated Databases

--- songs/models.py
+++ songs/models.py
@@ -9,6 +9,6 @@ class Song(models.Model):
     title = models.CharField(max_length=255)
     artist = models.CharField(max_length=255)
     album = models.CharField(max_length=255)
-    release_date = models.DateField()
+    release_date = models.DateField(blank=True)
     genre = models.CharField(max_length=255)
 
--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from songs.models import Song
+from songs.serializers import SongSerializer
+
+@api_view(['GET'])
+def songs_list(request):
+
+
+
+    return Response('ok')
